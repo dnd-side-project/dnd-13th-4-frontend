90077c99c89bcc6a3d1f5a283651e08a6b3d918a
Parksangwoo <psu8430@gmail.com>
2025-08-12 23:15:56 +0900

    feat: Stack 화면들을 상수로 관리하도록 구조 개선

---
+++ constants/Routes.ts
@@ -0,0 +1,14 @@
+export const STACK_SCREENS = [
+  {
+    name: '(tabs)',
+    options: { headerShown: false },
+  },
+  {
+    name: 'CreateMindLetter',
+    options: { title: '편지 쓰기' },
+  },
+  {
+    name: '+not-found',
+    options: {},
+  },
+] as const;

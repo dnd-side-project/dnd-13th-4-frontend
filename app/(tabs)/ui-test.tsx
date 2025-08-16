ae27af4a000874a368c4a965b80e715b00c75a49
caseBread <kgu0515@gmail.com>
2025-08-17 00:28:00 +0900

    fix : ui테스트페이지 수정

--- app/(tabs)/ui-test.tsx
+++ app/(tabs)/ui-test.tsx
@@ -8,27 +8,21 @@ import { StyleSheet, View } from 'react-native';
 const UiTest = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <RoundButton text='큰 소리로 노래했어요' />
-        <RoundButton text='큰 소리로 노래했어요' active />
-      </View>
-      <View>
-        <SquareButton text='중요한 회의 중' />
-        <SquareButton text='중요한 회의 중' active />
-      </View>
-      <View>
-        <LongSquareButton text='편하게 이야기 나눠보고 싶어요🌼' active />
-        <LongSquareButton text='편하게 이야기 나눠보고 싶어요🌼' />
-      </View>
-      <View>
-        <StatusButton text='30분 후' />
-        <StatusButton text='30분 후' active />
-      </View>
-      <View>
-        <CTAButton text='이전' />
-        <CTAButton text='다음' active />
-        <CTAButton text='다음' active disabled />
-      </View>
+      <RoundButton text='큰 소리로 노래했어요' />
+      <RoundButton text='큰 소리로 노래했어요' active />
+
+      <SquareButton text='중요한 회의 중' />
+      <SquareButton text='중요한 회의 중' active />
+
+      <LongSquareButton text='편하게 이야기 나눠보고 싶어요🌼' active />
+      <LongSquareButton text='편하게 이야기 나눠보고 싶어요🌼' />
+
+      <StatusButton text='30분 후' />
+      <StatusButton text='30분 후' active />
+
+      <CTAButton text='이전' />
+      <CTAButton text='다음' active />
+      <CTAButton text='다음' active disabled />
     </View>
   );
 };
@@ -36,5 +30,10 @@ const UiTest = () => {
 export default UiTest;
 
 const styles = StyleSheet.create({
-  container: { width: '100%' },
+  container: {
+    width: '100%',
+    backgroundColor: '#ffffff',
+    display: 'flex',
+    gap: 4,
+  },
 });

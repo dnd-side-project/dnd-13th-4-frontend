945745f4a5688fdb62d7869bb6abc58e8aba1a9d
caseBread <kgu0515@gmail.com>
2025-08-22 21:00:28 +0900

    feat : api 공톨 호출 로직 구현

--- app/(tabs)/notes/feeling.tsx
+++ app/(tabs)/notes/feeling.tsx
@@ -8,7 +8,9 @@ import { EMOTION_MOCK_LIST } from '@/components/notes/constants/mockData';
 import NoteCreateGuide from '@/components/notes/feeling/NoteCreateGuide';
 import NoteCreateFeelingHeader from '@/components/notes/feeling/NoteCreateHeaderLayout';
 import { GreyColors, PrimaryColors } from '@/constants/Colors';
+import { api } from '@/lib/api';
 import { useNoteCreateStore } from '@/store/note-create.store';
+import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'expo-router';
 import { StyleSheet, useWindowDimensions, View } from 'react-native';
 
@@ -16,10 +18,36 @@ const Feeling = () => {
   const router = useRouter();
   const { width: screenWidth } = useWindowDimensions();
   const { emotion, setEmotion } = useNoteCreateStore();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['/templates/situations', 'positive'],
+    queryFn: async () => {
+      const { data } = await api.get<{
+        id: number;
+        emotionType: string;
+        text: string;
+      }>({
+        path: '/templates/situations',
+        params: { emotionType: 'positive' },
+      });
+
+      return data;
+    },
+  });
 
   const changeEmotion = (newIndex: number): void => {
     setEmotion(EMOTION_MOCK_LIST[newIndex]);
   };
+
+  if (isLoading) {
+    /** 로딩 UI */
+    return null;
+  }
+  if (isError || !data) {
+    /** 에러 UI */
+    return null;
+  }
+
+  console.log(data);
 
   return (
     <SafeScreenLayout
---
+++ lib/api/index.ts
@@ -0,0 +1,40 @@
+import { buildQuery } from '../buildQuery';
+
+// api.ts
+type RequestOptions = {
+  path: string;
+  params?: Record<string, string | number | boolean>;
+  headers?: Record<string, string>;
+};
+
+const API_BASE_URL = 'http://wini.my';
+
+async function request<T>(
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  { path, params, headers }: RequestOptions,
+): Promise<{ data: T; status: number }> {
+  const url = `${API_BASE_URL}${path}${buildQuery(params)}`;
+
+  const res = await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`API Error: ${res.status} ${res.statusText}`);
+  }
+
+  const json: { data: T; status: number } = await res.json();
+
+  return json;
+}
+
+export const api = {
+  get: <T>(options: RequestOptions) => request<T>('GET', options),
+  post: <T>(options: RequestOptions) => request<T>('POST', options),
+  put: <T>(options: RequestOptions) => request<T>('PUT', options),
+  delete: <T>(options: RequestOptions) => request<T>('DELETE', options),
+};

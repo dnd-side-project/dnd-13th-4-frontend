b7974d24efe6976c942a5aad042c5a4f9c3aecae
caseBread <kgu0515@gmail.com>
2025-08-24 20:09:39 +0900

    feat : 마이페이지 퍼블리싱중

--- app/(tabs)/MyPage.tsx
+++ app/(tabs)/MyPage.tsx
@@ -1,6 +1,6 @@
 import { CustomText } from '@/components/CustomText';
 import { SafeScreenLayout } from '@/components/layout/SafeScreenLayout';
-import { PrimaryColors } from '@/constants/Colors';
+import { GreyColors, PrimaryColors } from '@/constants/Colors';
 import { StyleSheet, View } from 'react-native';
 
 export default function MyPage() {
@@ -13,18 +13,41 @@ export default function MyPage() {
       }
       background={{
         type: 'gradient',
-        colors: [PrimaryColors.blue300, '#ffffff'],
+        colors: [PrimaryColors.blue300, '#ffffff', '#ffffff'],
       }}
     >
-      <View>
-        <View>
-          <CustomText>이미지</CustomText>
-          <CustomText>고지우</CustomText>
+      <View style={styles.container}>
+        <View style={styles.profile}>
+          <View style={styles.profileImage} />
+          <CustomText variant='head1'>고지우</CustomText>
+        </View>
+        <View style={styles.roommateDataContainer}>
+          <CustomText
+            style={styles.roommateDataTitle}
+            variant='body2'
+            fontWeight='bold'
+          >
+            룸메이트 정보
+          </CustomText>
+          <View style={styles.roommateData}>
+            <View style={styles.roommateImage} />
+            <CustomText
+              style={styles.roommateName}
+              variant='body1'
+              fontWeight='semibold'
+            >
+              김예림
+            </CustomText>
+            <CustomText variant='body3' color={GreyColors.grey500}>
+              우리가 함께한 지 58일째
+            </CustomText>
+          </View>
         </View>
         <View>
-          <View>
-            <CustomText>룸메이트 정보</CustomText>
-          </View>
+          <CustomText>초대코드 복사</CustomText>
+        </View>
+        <View>
+          <CustomText>버전 정보</CustomText>
         </View>
       </View>
     </SafeScreenLayout>
@@ -38,14 +61,34 @@ const styles = StyleSheet.create({
     fontFamily: 'Pretendard-Bold',
     color: '#000',
   },
-  contentArea: {
-    backgroundColor: '#f5f5f5',
-    padding: 20,
+  container: { marginTop: 32 },
+  profile: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: 8,
+    paddingBottom: 32,
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    backgroundColor: PrimaryColors.blue100,
+    borderRadius: 999,
+  },
+  roommateDataContainer: {
+    backgroundColor: PrimaryColors.blue300,
     borderRadius: 12,
+    padding: 16,
   },
-  content: {
-    fontSize: 16,
-    fontFamily: 'Pretendard',
-    color: '#333',
+  roommateDataTitle: {
+    paddingBottom: 16,
   },
+  roommateImage: {
+    width: 36,
+    height: 36,
+    backgroundColor: PrimaryColors.blue100,
+    borderRadius: 999,
+    marginRight: 12,
+  },
+  roommateData: { flexDirection: 'row', alignItems: 'center' },
+  roommateName: { paddingRight: 8 },
 });

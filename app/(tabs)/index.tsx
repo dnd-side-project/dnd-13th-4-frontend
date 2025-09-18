79d3e323d4693eda35f9c239dd0e4bcb4b8c5e29
Parksangwoo <psu8430@gmail.com>
2025-09-19 01:17:33 +0900

    feat: 홈 화면에서 Overflow가 발생하는 경우 스크롤이 생기도록 수정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,21 +4,31 @@ import StatisticsSummary from '@/components/home/StatisticsSummary';
 import { StatusManager } from '@/components/home/StatusManager';
 import { TodayLetters } from '@/components/home/TodayLetters';
 import { SafeScreenLayout } from '@/components/layout/SafeScreenLayout';
+import { ScrollView } from 'react-native';
 
 export default function Home() {
   return (
     <>
       <SafeScreenLayout
-        header={<HomeHeader />}
         background={{
           type: 'gradient',
           colors: ['#ECF5FF', '#FFFFFF'] as const,
           locations: [0.149, 0.4087] as const,
         }}
+        style={{
+          paddingTop: 56,
+        }}
       >
-        <TodayLetters />
-        <RoommateStatus />
-        <StatisticsSummary />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          bounces={false}
+          overScrollMode='never'
+        >
+          <HomeHeader />
+          <TodayLetters />
+          <RoommateStatus />
+          <StatisticsSummary />
+        </ScrollView>
         <StatusManager />
       </SafeScreenLayout>
     </>
